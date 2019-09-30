7a01a228b289d7501a4cd6d564f09f3a00409a3c
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-29 21:00:06 -0600

    feat: generates Bing Site Verification XML documents

---
+++ gatsby-node.ts
@@ -0,0 +1,33 @@
+import builder from "xmlbuilder";
+import * as path from "path";
+import * as fs from "fs";
+import { promisify } from "util";
+
+const publicPath = "./public";
+const defaultXmlFileName = "BingSiteAuth.xml";
+
+interface PluginOptions {
+  userIds: string[];
+  xmlFileName: string;
+}
+
+export const onPostBuild = async (_: any, pluginOptions: PluginOptions) => {
+  if (pluginOptions && pluginOptions.userIds) {
+    const xml = builder
+      .create({
+        users: pluginOptions.userIds.map(userId => ({
+          user: {
+            "#text": userId
+          }
+        }))
+      })
+      .end();
+
+    const outputPath = path.join(
+      publicPath,
+      pluginOptions.xmlFileName || defaultXmlFileName
+    );
+
+    await promisify(fs.writeFile)(outputPath, xml);
+  }
+};
