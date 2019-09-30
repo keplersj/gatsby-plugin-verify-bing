b27d703ef9f950250b153db66cab31ebd302a744
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-29 21:03:48 -0600

    test: ignore build files

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   projects: [
     {
       displayName: "lint:prettier",
-      preset: "jest-runner-prettier"
+      preset: "jest-runner-prettier",
+      testPathIgnorePatterns: [
+        "/node_modules/",
+        "/gatsby-node.js",
+        "/gatsby-node.js.map",
+        "/gatsby-node.d.ts",
+        "/gatsby-node.d.ts.map"
+      ]
     }
   ]
 };
