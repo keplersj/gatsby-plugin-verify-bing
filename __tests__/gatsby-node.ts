58fdfcc7263d1b5e42f87250f81e45f9dc19be34
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-01 14:43:56 -0600

    test: xml gen for required and optional options

--- __tests__/gatsby-node.ts
+++ __tests__/gatsby-node.ts
@@ -1,23 +1,30 @@
 describe("gatsby-plugin-verify-bing: gatsby-node", () => {
-  it("generates a valid XML file, given valid options", async () => {
+  describe("generates a valid XML file", () => {
     const mockedWriteFile = jest.fn((name, contents, callback) => {
       callback();
     });
 
-    jest.mock("fs", () => ({
-      writeFile: mockedWriteFile
-    }));
+    beforeEach(() => {
+      jest.mock("fs", () => ({
+        writeFile: mockedWriteFile
+      }));
+    });
 
-    const { onPostBuild } = await import("../gatsby-node");
+    afterEach(() => {
+      mockedWriteFile.mockClear();
+    });
 
-    await onPostBuild(
-      {},
-      { userIds: ["testTESTtest"], xmlFileName: "BingSiteAuth.xml" }
-    );
+    it("given all options", async () => {
+      const { onPostBuild } = await import("../gatsby-node");
 
-    expect(mockedWriteFile).toHaveBeenCalledTimes(1);
-    expect(mockedWriteFile.mock.calls[0][0]).toBe("public/BingSiteAuth.xml");
-    expect(mockedWriteFile.mock.calls[0][1]).toMatchInlineSnapshot(`
+      await onPostBuild(
+        {},
+        { userIds: ["testTESTtest"], xmlFileName: "BingSiteAuth.xml" }
+      );
+
+      expect(mockedWriteFile).toHaveBeenCalledTimes(1);
+      expect(mockedWriteFile.mock.calls[0][0]).toBe("public/BingSiteAuth.xml");
+      expect(mockedWriteFile.mock.calls[0][1]).toMatchInlineSnapshot(`
       <?xml version="1.0"?>
       <users>
           <user>
@@ -25,5 +32,23 @@ describe("gatsby-plugin-verify-bing: gatsby-node", () => {
           </user>
       </users>
     `);
+    });
+
+    it("given only required options", async () => {
+      const { onPostBuild } = await import("../gatsby-node");
+
+      await onPostBuild({}, { userIds: ["testTESTtest"] });
+
+      expect(mockedWriteFile).toHaveBeenCalledTimes(1);
+      expect(mockedWriteFile.mock.calls[0][0]).toBe("public/BingSiteAuth.xml");
+      expect(mockedWriteFile.mock.calls[0][1]).toMatchInlineSnapshot(`
+      <?xml version="1.0"?>
+      <users>
+          <user>
+              testTESTtest
+          </user>
+      </users>
+    `);
+    });
   });
 });
